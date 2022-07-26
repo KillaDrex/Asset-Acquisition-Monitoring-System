7c0a201161e92dd8eba2597e17306e03fd2639b8
Jomylad <104539292+Jomylad@users.noreply.github.com>
2022-07-26 16:36:52 +0800

    Add files via upload

---
+++ Asset Acquistion & Monitoring System/src/MyLibs/Manager.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package MyLibs;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author RONALD
+ */
+public class Manager {
+      
+    private String Manager;
+    private String Employee;
+    
+    
+    public static void Employee(String[] args) {
+    ArrayList<Equipment> assignEquipment = new ArrayList<Equipment>();
+    System.out.println(Equipment);
+  }
+   public static void Report (String[] args) {
+    ArrayList<Equipment> generateReport = new ArrayList<Report>();
+    System.out.println(Equipment);
+  } 
+    
+}
