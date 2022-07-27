4aabe9bf1bc2a31bbc324ece0d20849937dfebc4
Abero Geronga (Blake) <104506995+abero-geronga@users.noreply.github.com>
2022-07-27 18:05:39 +0800

    Add files via upload

---
+++ Asset Acquistion & Monitoring System/src/MyLibs/Report.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package MyLibs;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author abero
+ */
+public abstract class Report {
+    private ArrayList <Equipment> equipmentList;
+    
+    public Report (ArrayList<equipmentList>){
+        this.equipmentList = equipmentList;
+    }
+    
+    public abstract void printDetails();
+}
