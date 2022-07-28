fd13a5faa471d7e44d08744499fc505f827ebcc3
yaems <101038225+yaems@users.noreply.github.com>
2022-07-28 15:45:02 +0800

    Create Date.java

---
+++ Asset Acquistion & Monitoring System/src/MyLibs/Date.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package MyLibs;
+
+/**
+ *
+ * @author jmsde
+ */
+public class Date {
+    private int day;
+    private int month;
+    private int year;
+
+    public Date(int day, int month, int year) {
+        this.day = day;
+        this.month = month;
+        this.year = year;
+    }
+
+}
