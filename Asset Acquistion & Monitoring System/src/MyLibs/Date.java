eaf7ee4895f3047bd290225f5fc875a40661b5fd
yaems <101038225+yaems@users.noreply.github.com>
2022-07-28 17:20:59 +0800

    Update Date.java

--- Asset Acquistion & Monitoring System/src/MyLibs/Date.java
+++ Asset Acquistion & Monitoring System/src/MyLibs/Date.java
@@ -9,14 +9,15 @@ package MyLibs;
  * @author jmsde
  */
 public class Date {
+    
     private int day;
     private int month;
     private int year;
 
     public Date(int day, int month, int year) {
-        this.day = day;
-        this.month = month;
-        this.year = year;
-    }
+    this.day = day;
+    this.month = month;
+    this.year = year;
+  }
 
 }
